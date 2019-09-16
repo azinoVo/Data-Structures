d4ce8c6fbe8dc27c8afbc2c84b51302881ab0005
azinoVo <azinovo22@gmail.com>
2019-09-16 17:18:00 -0500

    added the linked list func from lecture

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -8,11 +8,14 @@ class Queue:
     # Why is our DLL a good choice to store our elements?
     self.storage = 
 
+# Add an item to the back of the queue
   def enqueue(self, value):
     pass
-  
+
+# Remove and return an item from the front of the queue
   def dequeue(self):
     pass
 
+# Returns the number of items in the queue
   def len(self):
     pass
