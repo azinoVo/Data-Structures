c4548296cccfa907f4590f74cae96d19f17bfc27
azinoVo <azinovo22@gmail.com>
2019-09-16 22:19:48 -0500

    general implem for queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,21 +1,28 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+# First In, First Out
 class Queue:
   def __init__(self):
     self.size = 0
     # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    self.storage = DoublyLinkedList()
 
 # Add an item to the back of the queue
   def enqueue(self, value):
-    pass
+    self.size += 1
+    self.storage.add_to_tail(value)
 
 # Remove and return an item from the front of the queue
   def dequeue(self):
-    pass
+    if self.size > 0:
+        self.size -= 1
+        self.storage.remove_from_head()
+    else:
+        pass
 
 # Returns the number of items in the queue
   def len(self):
-    pass
+    return self.size
+
+Queue().enqueue(3)
+print(Queue().storage.head)
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,12 +2,13 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+# Stack is last-in, first-out
 class Stack:
   def __init__(self):
     self.size = 0
     # Why is our DLL a good choice to store our elements?
-    self.storage = 
-
+    self.storage = DoublyLinkedList()
+    
   def push(self, value):
     pass
   
