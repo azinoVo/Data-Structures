e902240d5b3744dda535c4b4420d71946db3de43
azinoVo <azinovo22@gmail.com>
2019-09-18 21:00:43 -0500

    foundation code

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -23,7 +23,19 @@ class BinarySearchTree:
   # rules of the ordering of elements in a binary search tree.
   # Need to traverse to find spot to insert
   def insert(self, value):
-    pass
+    new_node = BinarySearchTree(value)
+    # If there's nodes
+    if value >= self.value: # Greater than current node, traverse and go right - check again
+      # if there isn't a right, set self.right equal new_node
+      if not self.right:
+          self.right = new_node
+      else:
+        self.right.insert(value)  
+    elif value < self.value: # Less than current node, traverse and go left - check again
+      if not self.left:
+        self.left = new_node
+      else:
+        self.left.insert(value)
 
   # * `contains` searches the binary search tree for the input value, 
   # returning a boolean indicating whether the value exists in the tree or not.
@@ -31,14 +43,20 @@ class BinarySearchTree:
   # We can stop at the first instance of a value
   # We know it's not found if we get to a node that doesn't have children
   def contains(self, target):
+    # Traverse and check left and right nodes depending on current_node
     pass
 
   # * `get_max` returns the maximum value in the binary search tree.
   def get_max(self):
+    # Go to the node all the way to the right
     pass
 
   # * `for_each` performs a traversal of _every_ node in the tree, executing
   # the passed-in callback function on each tree node value. There is a myriad of ways to
   # perform tree traversal; in this case any of them should work. 
   def for_each(self, cb):
-    pass 
+    cb(self.value)
+    if self.right:
+      self.right.for_each(cb)
+    if self.left:
+      self.left.for_each(cb)
